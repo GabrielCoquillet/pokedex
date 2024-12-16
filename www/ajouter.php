d3ddffd114a8a8a09f0c2415286e0befd4b14523
Gabriel Coquillet <163413154+GabrielCoquillet@users.noreply.github.com>
2024-12-16 08:18:17 +0100

    Dev (#2)
    * Create bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * Update bdd_schematic
    
    * added database creation file
    
    * Update bdd_schematic
    
    * Update BDD_creation.sql
    
    * Update bdd_schematic
    
    * Update BDD_creation.sql
    
    * Update BDD_creation.sql
    
    * Update bdd_schematic
    
    * Update BDD_creation.sql
    
    * Update BDD_creation.sql
    
    * Update BDD_creation.sql
    
    * Update bdd_schematic
    
    * Create d
    
    * Add files via upload
    
    * Delete www/d
    
    * update
    
    * update
    
    * update

---
+++ www/ajouter.php
@@ -0,0 +1,20 @@
+<!-- contenu de la page rechercher -->
+<h1>Ajouter une BD</h1>
+<div class="formulaire">
+    <form action="index.php?a=ajouter" method="post">
+        <fieldset>
+            <legend>Informations de la BD à ajouter</legend>
+            <p>
+                <label for="titre">Titre : </label>
+                <input type="text" name="titre" id="titre" value="" placeholder="Titre"\>
+                <label for="auteur">Auteur : </label>
+                <input type="text" name="auteur" id="auteur" value="" placeholder="Auteur"\>
+            </p>
+            <p>
+                <label for="image_bd">Téléchargez une image : </label>
+                <input type="file" name="image_bd" id="image_bd" value=""/>
+            </p>
+        </fieldset>
+        <p class="center"><input type="submit" value="Lister"/> <input type="reset" value="Effacer les champs"/></p>
+    </form>
+</div>
---
+++ www/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pokedex</title>
+    <link rel="icon" type="image/png" href="" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<?php include("config.php"); ?>
+<!-- titre -->
+<div id="div_titre">
+    <h1>Pokedex</h1>
+</div>
+<div class="clear"></div>
+
+<!-- menu -->
+<div id="menu">
+    <ul id="onglets">
+        <li><a href="index.php?a=lister">Liste des Pokémons</a></li>
+        <li><a href="index.php?a=rechercher">Rechercher un Pokémon</a></li>
+        <li><a href="index.php?a=ajouter">Ajouter un Pokémon</a></li>
+    </ul>
+</div>
+
+<!-- contenu de la page -->
+<div id="contenu">
+    <?php
+    //orientation vers la bonne page
+    if (isset($_GET['a']) && !empty($_GET['a'])) {
+        //Si l'action est specifiée, on l'utilise, sinon, on tente une action par défaut
+        $action = (!empty($_GET['a'])) ? $_GET['a'].'.php' : 'index.php';
+
+        // Si l'action existe, on l'exécute
+        if (is_file($action)) {
+            include $action;
+            // Sinon, on affiche la page d'index du module !
+        }
+        // Module non specifié ou invalide ? On affiche la page d'accueil !
+    }
+    else {
+        include "lister.php";
+    }
+    ?>
+</div>
+
+<!-- Pied de page -->
+<div id="footer">
+    Jallet Kishokumar Coquillet Décembre 2024
+</div>
+</body>
+</html>
+
